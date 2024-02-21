d9e42dbcb66728493af3f77a3f4b5def3db853d4
gustavocadev <gustavocadev@gmail.com>
2024-02-20 22:15:42 -0500

    Add create_task route to tasks_routes.rs

--- src/routes/tasks_routes.rs
+++ src/routes/tasks_routes.rs
@@ -1,7 +1,9 @@
-use crate::controllers::tasks_controller::get_tasks;
-use axum::routing::get;
+use crate::controllers::tasks_controller::{create_task, get_tasks};
+use axum::routing::{get, post};
 use axum::Router;
 
 pub fn init_tasks_routes() -> Router {
-  Router::new().route("/", get(get_tasks))
+  Router::new()
+    .route("/", get(get_tasks))
+    .route("/", post(create_task))
 }
