2aa48ebec3e1e2823df2c63847b169e3b9bbff74
gustavocadev <gustavocadev@gmail.com>
2024-02-21 12:31:00 -0500

    Add server port print statement

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ async fn main() {
   let app = Router::new().nest("/api/tasks", init_tasks_routes());
 
   // create a new listener
+  println!("Server running on port {}", 3000);
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
   axum::serve(listener, app).await.unwrap();
 }
