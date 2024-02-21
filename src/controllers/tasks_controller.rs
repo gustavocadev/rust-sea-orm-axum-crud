fc271c8f68b9100b8a4b0e1a5e65a1aa0b2ed733
gustavocadev <gustavocadev@gmail.com>
2024-02-21 12:30:53 -0500

    Refactor tasks controller and add CRUD operations

--- src/controllers/tasks_controller.rs
+++ src/controllers/tasks_controller.rs
@@ -1,23 +1,120 @@
-use axum::Json;
-use entity::post;
-use sea_orm::{ActiveModelTrait, Set};
+use crate::lib::db::{self, db_connection};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
+use entity::task::{self, Entity as Task};
+use sea_orm::{ActiveModelTrait, DeleteResult, EntityTrait, ModelTrait, Set};
+use serde::Deserialize;
+use serde_json::json;
 
-use crate::lib::db::db_connection;
-
-pub async fn get_tasks() -> Json<&'static str> {
+pub async fn get_tasks() -> impl IntoResponse {
   let db = db_connection().await.unwrap();
 
-  let post = post::ActiveModel {
-    title: Set(String::from("Amazing title 1")),
-    text: Set(String::from("Lorem ipsum dolor sit amet.")),
-    ..Default::default()
-  };
+  let tasks = Task::find().all(&db).await.unwrap();
 
-  let post: post::Model = post.insert(&db).await.unwrap();
-  println!("Post created with ID: {}, TITLE: {}", post.id, post.title);
-  Json("Hello, World! 🦀")
+  // Convert tasks to a Vec<serde_json::Value>
+  let tasks = tasks
+    .into_iter()
+    .map(|task| {
+      serde_json::json!({
+        "id": task.id,
+        "title": task.title,
+        "description": task.description
+      })
+    })
+    .collect::<Vec<serde_json::Value>>();
+
+  Json(serde_json::json!(tasks))
 }
 
-pub async fn create_task() -> Json<&'static str> {
-  Json("Create a new task! 🦀")
+pub async fn get_task(Path(id): Path<u16>) -> impl IntoResponse {
+  let db = db_connection().await.unwrap();
+
+  let task = Task::find_by_id(id).one(&db).await.unwrap();
+
+  let task = task
+    .into_iter()
+    .map(|task| {
+      serde_json::json!({
+        "id": task.id,
+        "title": task.title,
+        "description": task.description
+      })
+    })
+    .collect::<Vec<serde_json::Value>>();
+
+  Json(serde_json::json!(task))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTask {
+  title: String,
+  description: String,
+}
+
+pub async fn create_task(Json(body): Json<CreateTask>) -> impl IntoResponse {
+  let db = db_connection().await.unwrap();
+
+  let task = task::ActiveModel {
+    title: Set(String::from(body.title)),
+    description: Set(String::from(body.description)),
+    ..Default::default() // all other attributes are `NotSet`
+  };
+
+  let task: task::Model = task.insert(&db).await.unwrap();
+
+  (
+    StatusCode::CREATED,
+    Json(serde_json::json!({
+      "id": task.id,
+      "title": task.title,
+    })),
+  )
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateTask {
+  title: String,
+  description: String,
+}
+
+pub async fn update_task(
+  Path(id): Path<u16>,
+  Json(body): Json<UpdateTask>,
+) -> Result<impl IntoResponse, Json<serde_json::Value>> {
+  println!("ID: {}", id);
+
+  println!("Body: {:?}", body);
+
+  let db = db_connection().await.unwrap();
+
+  // UPDATE title of Post by ID
+  let task: Option<task::Model> = Task::find_by_id(id).one(&db).await.unwrap();
+
+  // transform Option<task::Model> to task::ActiveModel
+  let mut task: task::ActiveModel = task.unwrap().into();
+
+  task.title = Set(body.title.to_owned());
+  task.description = Set(body.description.to_owned());
+
+  let task = task.update(&db).await.unwrap();
+
+  println!("Post updated with ID: {}, TITLE: {}", task.id, task.title);
+
+  Ok(Json(json!({ "message": "Task updated!" })))
+}
+
+pub async fn delete_taks(Path(id): Path<u16>) -> Result<impl IntoResponse, StatusCode> {
+  let db = db_connection().await.unwrap();
+
+  // DELETE Post by ID
+  let task = Task::find_by_id(id).one(&db).await.unwrap();
+  let task = task.unwrap();
+
+  let res: DeleteResult = task.delete(&db).await.unwrap();
+  assert_eq!(res.rows_affected, 1);
+
+  println!("Post deleted: {:?}", res);
+
+  Ok(Json(json! ({
+    "msg": "Task deleted! 🦀",
+  })))
 }
