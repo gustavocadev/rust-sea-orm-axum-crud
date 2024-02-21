f0d3031dfbe8bbb02d37a82a97030d309dfe837e
gustavocadev <gustavocadev@gmail.com>
2024-02-20 22:13:31 -0500

    Add post creation functionality to tasks controller

--- src/controllers/tasks_controller.rs
+++ src/controllers/tasks_controller.rs
@@ -1,5 +1,23 @@
 use axum::Json;
+use entity::post;
+use sea_orm::{ActiveModelTrait, Set};
+
+use crate::lib::db::db_connection;
 
 pub async fn get_tasks() -> Json<&'static str> {
-  Json("Hello, World! This is the tasks route!")
+  let db = db_connection().await.unwrap();
+
+  let post = post::ActiveModel {
+    title: Set(String::from("Amazing title 1")),
+    text: Set(String::from("Lorem ipsum dolor sit amet.")),
+    ..Default::default()
+  };
+
+  let post: post::Model = post.insert(&db).await.unwrap();
+  println!("Post created with ID: {}, TITLE: {}", post.id, post.title);
+  Json("Hello, World! 🦀")
+}
+
+pub async fn create_task() -> Json<&'static str> {
+  Json("Create a new task! 🦀")
 }
