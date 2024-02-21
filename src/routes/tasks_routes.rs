f61f142c0cc98169881776f3b83f1c3e5f370faf
gustavocadev <gustavocadev@gmail.com>
2024-02-21 12:30:43 -0500

    Update tasks_routes.rs with new controller functions

--- src/routes/tasks_routes.rs
+++ src/routes/tasks_routes.rs
@@ -1,9 +1,11 @@
-use crate::controllers::tasks_controller::{create_task, get_tasks};
-use axum::routing::{get, post};
+use crate::controllers::tasks_controller::{
+  create_task, delete_taks, get_task, get_tasks, update_task,
+};
+use axum::routing::{get, patch};
 use axum::Router;
 
 pub fn init_tasks_routes() -> Router {
   Router::new()
-    .route("/", get(get_tasks))
-    .route("/", post(create_task))
+    .route("/", get(get_tasks).post(create_task))
+    .route("/:id", patch(update_task).delete(delete_taks).get(get_task))
 }
